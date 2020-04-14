be6e119ae5ee6f4f14ba533667f9c9b78f085081
Manuel Cepeda <mecm1993@gmail.com>
2020-04-13 21:15:32 -0400

    chore(style): remove unused css and logo

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import './assets/styles/main.scss';
 import * as serviceWorker from './serviceWorker';
